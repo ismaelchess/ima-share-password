d86c4b0cec944be6a32084a5d7a4c2076a3cd5bb
mauleyzaola <ismael.martinez.a@gmail.com>
2021-02-03 19:03:52 -0600

    SS-3 form to input data

--- cmd/share-secret/main.go
+++ cmd/share-secret/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+type Secret struct {
+	SecretValue string `json:"secretvalue"`
+	SecretTime  int    `json:"secrettime"`
+}
 
 func main() {
 	if err := run(); err != nil {
@@ -13,12 +21,40 @@ func main() {
 }
 
 func run() error {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := fmt.Fprintln(w, "hello world"); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	r.HandleFunc("/secret", sharesecretpost).Methods(http.MethodPost)
+	r.HandleFunc("/secret/{key}", sharesecretlink).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 	return nil
+}
+
+func sharesecretpost(w http.ResponseWriter, r *http.Request) {
+
+	var secretData Secret
+	json.NewDecoder(r.Body).Decode(&secretData)
+
+	fmt.Println(r.Body)
+	fmt.Println(secretData)
+	secretData.SecretTime = 0
+
+	json.NewEncoder(w).Encode(secretData)
+}
+
+func sharesecretlink(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("xxxxxxx")
+
+	// var secretData Secret
+	// json.NewDecoder(r.Body).Decode(&secretData)
+	// fmt.Println(r.Body)
+	// fmt.Println(secretData)
 }
